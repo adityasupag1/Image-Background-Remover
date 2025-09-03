c24e8fdbbdf476b420329446dead95df1a38ec95
Aditya Raj <araj15397@gmail.com>
2025-09-03 18:19:36 +0530

    testimonial responsive

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -24,7 +24,7 @@ const TestimonialCard = (props) => {
 const {pic, name, desc, role} = props.testimonial;
 
   return (
-    <div className="bg-white rounded-xl p-6 max-sm:flex-col-reverse drop-shadow-md max-w-lg m-auto hover:scale-105 transition-all duration-700 max-sm:min-w-[250px] h-auto my-4">
+    <div className="bg-white rounded-xl p-6 max-sm:flex-col-reverse drop-shadow-md  m-auto hover:scale-105 transition-all duration-700 max-w-[500px] h-auto my-4 sm:mx-4">
       <p className="text-4xl text-gray-500">”</p>
       <p className="text-sm text-gray-500">{desc}
       </p>
@@ -52,7 +52,7 @@ const {pic, name, desc, role} = props.testimonial;
           Customer Testimonials
         </div>
         {/* Testimonial cards */}
-        <div className="flex flex-wrap justify-center max-sm:mx-[20%] lg:mx-50">
+        <div className="flex flex-wrap justify-center max-sm:mx-4">
              {
               CutsomerReviwData.map((testimonial) =>(
                 <TestimonialCard key= {testimonial.id} testimonial = {testimonial}/>
